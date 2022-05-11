ee4980540cf029c3e7bfb16e685d1adf2416dc66
Sandeep Pareek <psandeep01@gmail.com>
2022-05-11 15:10:43 +0530

    Department service

---
+++ src/main/java/com/sandeep/departmentservice/Controller/Controller.java
@@ -0,0 +1,28 @@
+package com.sandeep.departmentservice.Controller;
+
+import com.sandeep.departmentservice.Entity.Department;
+import com.sandeep.departmentservice.Service.DepartmentService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/department")
+@Slf4j
+public class Controller  {
+
+    @Autowired
+    private DepartmentService departmentService;
+
+    @PostMapping("/")
+    public Department saveDepartment(@RequestBody Department department){
+        log.info("Inside saveDepartment method of DepartmentController");
+        return departmentService.saveDepartment(department);
+    }
+
+    @GetMapping("/{id}")
+    public Department getDepartmentById(@PathVariable("id") Long departmentId){
+        return departmentService.getDepartmentById(departmentId);
+    }
+
+}
